35aeba68c77a70240f3e6d8c6d8e2c4df48e5f3b
prince kumar <princeofficial0@gmail.com>
2025-04-28 00:01:37 +0530

    updated headers for download file

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import multer from "multer";
-import * as xlsx from "xlsx";
+import xlsx from "xlsx";
+//import { readFile, utils, writeFile } from "xlsx";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
+const { readFile, utils, writeFile } = xlsx; 
 const app = express();
 const PORT = 3000;
 
@@ -17,7 +19,7 @@ app.use(express.static("public"));
 // Multer config
 const upload = multer({ dest: "uploads/" });
 
-// GET route
+// GET routecd
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
@@ -31,23 +33,68 @@ app.post(
       const file1 = req.files.file1[0];
       const file2 = req.files.file2[0];
 
-      const workbook1 = xlsx.readFile(file1.path);
-      const workbook2 = xlsx.readFile(file2.path);
+      const workbook1 = readFile(file1.path);
+      const workbook2 = readFile(file2.path);
 
       const sheet1 = workbook1.Sheets[workbook1.SheetNames[0]];
       const sheet2 = workbook2.Sheets[workbook2.SheetNames[0]];
 
-      const cognism = xlsx.utils.sheet_to_json(sheet1);
-      const vincere = xlsx.utils.sheet_to_json(sheet2);
+      const cognism = utils.sheet_to_json(sheet1);
+      const vincere = utils.sheet_to_json(sheet2);
 
-      const filteredArray = cognism.filter(
-        (obj1) => !vincere.some((obj2) => obj2.email === obj1.email)
+      const vincereEmails = new Set(
+        vincere.map((obj) => obj.email).filter((email) => !!email)
+      );
+      const vincerePhones = new Set(
+        vincere.map((obj) => obj.phone).filter((phone) => !!phone)
       );
 
+      const filteredArray = cognism.filter((obj1) => {
+        if (obj1.Email) {
+          return !vincereEmails.has(obj1.Email);
+        } else if (obj1.Mobile) {
+          return !vincerePhones.has(obj1.Mobile);
+        } else {
+          return true;
+        }
+      });
+
+      // const filteredArray = cognism.filter(
+      //   (obj1) => !vincere.some((obj2) => obj2.email === obj1.email)
+      // );
+
       // Create new workbook with filtered data
-      const worksheet = xlsx.utils.json_to_sheet(filteredArray);
-      const workbook = xlsx.utils.book_new();
-      xlsx.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+      const headers = [
+        "contact-companyId",
+        "contact-externalId",
+        "contact-lastName",
+        "contact-middleName",
+        "contact-firstName",
+        "contact-email",
+        "contact-phone",
+        "contact-jobTitle",
+        "contact-linkedin",
+      ];
+
+      const vincereFormat = filteredArray.map((obj) => ({
+        "contact-companyId": "",
+        "contact-externalId": obj["Profile ID"] || "",
+        "contact-lastName": obj["Last Name"] || "",
+        "contact-middleName": "",
+        "contact-firstName": obj["First Name"] || "",
+        "contact-email": obj.Email || "",
+        "contact-phone": obj.Mobile || "",
+        "contact-jobTitle": obj["Job Title"] || "",
+        "contact-linkedin": obj["Personal Linkedin URL"] || "",
+      }));
+
+
+      const worksheet = utils.json_to_sheet(vincereFormat, {
+        header: headers,
+      });
+      const workbook = utils.book_new();
+      utils.book_append_sheet(workbook, worksheet, "Sheet1");
 
       const outputDir = path.join(__dirname, "filterContact");
       const outputPath = path.join(outputDir, "filteredContact.xlsx");
@@ -57,13 +104,18 @@ app.post(
         fs.mkdirSync(outputDir, { recursive: true });
       }
 
-      xlsx.writeFile(workbook, outputPath);
+      writeFile(workbook, outputPath);
 
       // Clean up uploads
       fs.unlinkSync(file1.path);
       fs.unlinkSync(file2.path);
 
-      res.download(outputPath);
+      res.download(outputPath, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        fs.unlinkSync(outputPath);
+      });
     } catch (error) {
       console.error("Error processing files:", error);
       res.status(500).send("An error occurred while processing the files.");

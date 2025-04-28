077d20dcb5283f5f62beeb38f5af15084c078fcb
Prince Kumar <77004940+Prince2548@users.noreply.github.com>
2025-04-28 09:35:10 +0100

    Update index.js
    back to xlsx

--- index.js
+++ index.js
@@ -103,8 +103,8 @@ app.post(
         fs.mkdirSync(outputDir, { recursive: true });
       }
 
-      //writeFile(workbook, outputPath);
-      fs.writeFileSync(outputPath.replace('.xlsx', '.csv'), utils.sheet_to_csv(worksheet));
+      writeFile(workbook, outputPath);
+      //fs.writeFileSync(outputPath.replace('.xlsx', '.csv'), utils.sheet_to_csv(worksheet));
 
       // Clean up uploads
       fs.unlinkSync(file1.path);
